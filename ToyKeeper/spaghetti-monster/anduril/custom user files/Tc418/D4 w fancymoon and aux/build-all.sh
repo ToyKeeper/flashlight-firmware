3592f20eb50a447fd62f15e483f97f0c03c8a2e7
Tom U <tommycat29@gmail.com>
2019-03-23 14:35:34 -0400

    added files for tc418 D4 w/ fancy moon and aux

---
+++ ToyKeeper/spaghetti-monster/anduril/custom user files/Tc418/D4 w fancymoon and aux/build-all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+UI=anduril
+
+for TARGET in cfg-emisar-d4.h ; do
+  NAME=$(echo "$TARGET" | perl -ne '/cfg-(.*).h/ && print "$1\n";')
+  echo "===== $NAME ====="
+  echo ../../../bin/build.sh 85 "$UI" "-DCONFIGFILE=${TARGET}"
+  ../../../bin/build.sh 85 "$UI" "-DCONFIGFILE=${TARGET}"
+  mv -f "$UI".hex "$UI".$NAME.hex
+done
