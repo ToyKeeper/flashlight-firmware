1b973de93d01d6c518598d8b9f66173b9b366075
Dan Nelson <dnelson_1901@yahoo.com>
2019-08-28 09:28:02 -0500

    default to just building for d4v2 since that's the light I have

--- ToyKeeper/spaghetti-monster/anduril/build-all.sh
+++ ToyKeeper/spaghetti-monster/anduril/build-all.sh
@@ -2,7 +2,8 @@
 
 UI=anduril
 
-for TARGET in cfg-*.h ; do
+#for TARGET in cfg-*.h ; do
+for TARGET in cfg-emisar-d4v2.h ; do
   NAME=$(echo "$TARGET" | perl -ne '/cfg-(.*).h/ && print "$1\n";')
   echo "===== $NAME ====="
   ATTINY=$(grep 'ATTINY:' $TARGET | awk '{ print $3 }')
@@ -10,4 +11,5 @@ for TARGET in cfg-*.h ; do
   echo ../../../bin/build.sh $ATTINY "$UI" "-DCONFIGFILE=${TARGET}"
   ../../../bin/build.sh $ATTINY "$UI" "-DCONFIGFILE=${TARGET}"
   mv -f "$UI".hex "$UI".$NAME.hex
+  mv -f "$UI".elf "$UI".$NAME.elf
 done
