37a5a0cc49de47a38a5eda8973950049671846e8
Marshall Flax <marshall.flax@gmail.com>
2020-11-22 10:29:48 -0500

    Add color highlights to fun mode

--- ToyKeeper/spaghetti-monster/anduril/fun-mode.c
+++ ToyKeeper/spaghetti-monster/anduril/fun-mode.c
@@ -28,26 +28,50 @@ PROGMEM const PWM_DATATYPE pattern[] = {0b11111100, 0b10001000, 0b10000000, 0b10
 inline void fun_strobe_iter() {
     const int16_t nominal_level = 
                             #ifdef USE_BIKE_FLASHER_MODE
-                            clamp16(bike_flasher_brightness, 1, STROBE_BRIGHTNESS/4);
+                            clamp16(bike_flasher_brightness, 
+                                    #if defined(USE_AUX_RGB_LEDS) 
+                                    0
+                                    #else
+                                    1
+                                    #endif
+                                    , STROBE_BRIGHTNESS/4);
                             #else
                             1;
                             #endif
 
     // one iteration of main loop()
+    uint8_t prior_brightness = 0;
     for(uint8_t i=0; i<PATTERN_SIZE; i++) {
         uint8_t code = PWM_GET(pattern, i);
-	for (uint8_t b = 0; b < 4; b++) {
-            set_level(((code & 0xC0) >> 6) * nominal_level);
-	    code = code << 2;
-	    nice_delay_ms(
-                            #ifdef USE_PARTY_STROBE_MODE
-			    strobe_delays[party_strobe_e]
-                            #else
-			    50
-                            #endif
-			    );
+
+        // four semi-nibbles per byte
+        for (uint8_t b = 0; b < 4; b++) {
+          uint8_t brightness = (code & 0xC0) >> 6;
+          if (brightness != prior_brightness) {
+              set_level(brightness * nominal_level);
+              #if defined(USE_AUX_RGB_LEDS) 
+              switch (brightness) {
+                case 1:  rgb_led_set(0x01 << 1); break;
+                case 2:  rgb_led_set(0x11 << 1); break;
+                case 3:  rgb_led_set(0x14 << 1); break;
+                default: rgb_led_set(0); break;
+              }
+              #endif
+              prior_brightness = brightness;
+          }
+          code = code << 2; 
+          nice_delay_ms(
+            #ifdef USE_PARTY_STROBE_MODE
+            strobe_delays[party_strobe_e]
+            #else
+            50
+            #endif
+          );
         }
     }
     set_level(0);
+    #if defined(USE_AUX_RGB_LEDS) 
+    rgb_led_set(0);
+    #endif
 } 
 #endif
